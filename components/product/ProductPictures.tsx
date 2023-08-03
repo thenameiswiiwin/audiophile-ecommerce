939b70a470a5a36dbd795d4768fc61d498e880c1
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-02 22:32:16 -0500

    feat(ProductPictures.tsx, Recommended.tsx): Component to display related products

--- components/product/ProductPictures.tsx
+++ components/product/ProductPictures.tsx
@@ -1,5 +1,63 @@
-const ProductPictures = ({ productArray }) => {
-  return <div>ProductPictures</div>
+import type { Product } from '@/types'
+
+const ProductPictures = ({ productArray }: { productArray: Product }) => {
+  return (
+    <div className="flex w-full flex-col gap-[20px] pb-[120px] md:flex-row md:gap-[18px] lg:gap-[30px] lg:pb-[160px]">
+      <div className="flex flex-col gap-[20px] lg:gap-[32px]">
+        <div className="w-full">
+          <img
+            className="h-[174px] w-full rounded-[8px] object-cover md:hidden"
+            src={productArray.gallery.first.mobile}
+            alt={`first-${productArray.name}`}
+          />
+          <img
+            className="hidden h-[174px] w-full min-w-[277px] rounded-[8px] object-cover md:block lg:hidden"
+            src={productArray.gallery.first.tablet}
+            alt={`first-${productArray.name}`}
+          />
+          <img
+            className="hidden h-[280px] w-full min-w-[445px] rounded-[8px] object-cover lg:block"
+            src={productArray.gallery.first.desktop}
+            alt={`first-${productArray.name}`}
+          />
+        </div>
+        <div className="w-full">
+          <img
+            className="h-[174px] w-full rounded-[8px] object-cover md:hidden"
+            src={productArray.gallery.second.mobile}
+            alt={`second-${productArray.name}`}
+          />
+          <img
+            className="hidden h-[174px] w-full min-w-[277px] rounded-[8px] object-cover md:block lg:hidden"
+            src={productArray.gallery.second.tablet}
+            alt={`second-${productArray.name}`}
+          />
+          <img
+            className="hidden h-[280px] w-full min-w-[445px] rounded-[8px] object-cover lg:block"
+            src={productArray.gallery.second.desktop}
+            alt={`second-${productArray.name}`}
+          />
+        </div>
+      </div>
+      <div className="w-full">
+        <img
+          className="h-[368px] w-full rounded-[8px] object-cover md:hidden"
+          src={productArray.gallery.third.mobile}
+          alt={`third-${productArray.name}`}
+        />
+        <img
+          className="hidden h-[368px] w-full min-w-[395px] rounded-[8px] object-cover md:block lg:hidden"
+          src={productArray.gallery.third.tablet}
+          alt={`third-${productArray.name}`}
+        />
+        <img
+          className="hidden h-[592px] w-full min-w-[635px] rounded-[8px] object-cover lg:block"
+          src={productArray.gallery.third.desktop}
+          alt={`third-${productArray.name}`}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default ProductPictures
--- components/product/Recommended.tsx
+++ components/product/Recommended.tsx
@@ -1,5 +1,48 @@
-const Recommended = ({ productArray }) => {
-  return <div>Recommended</div>
+import { nanoid } from 'nanoid'
+
+import type { Product } from '@/types'
+
+import Button from '../Button'
+
+const Recommended = ({ productArray }: { productArray: Product }) => {
+  return (
+    <div className="pb-[120px] lg:pb-[160px]">
+      <h2 className="pb-[40px] text-center text-[24px] font-bold uppercase leading-[36px] tracking-[0.86px] md:flex-1 md:pb-[56px] md:text-[32px] md:tracking-[1.14px] lg:flex-none lg:pb-[64px]">
+        You may also like
+      </h2>
+      <div className="flex w-full flex-col gap-[56px] md:flex-row md:gap-[11px] lg:gap-[30px]">
+        {productArray.others.map((other) => (
+          <div key={nanoid()} className="w-full">
+            <div className="flex w-full items-center justify-center rounded-[8px] bg-gray-base">
+              <img
+                className="max-h-[120px] rounded-[8px] object-cover md:hidden"
+                src={other.image.mobile}
+                alt={other.name}
+              />
+              <img
+                className="hidden max-h-[318px] rounded-[8px] object-cover md:block lg:hidden"
+                src={other.image.tablet}
+                alt={other.name}
+              />
+              <img
+                className="hidden max-h-[318px] rounded-[8px] object-cover lg:block"
+                src={other.image.desktop}
+                alt={other.name}
+              />
+            </div>
+            <article className="flex w-full flex-col items-center">
+              <h3 className="py-[32px] text-[24px] font-bold uppercase tracking-[1.71px] md:pt-[40px]">
+                {other.name}
+              </h3>
+              <Button href={`/${other.slug}`} intent="primary" size="base">
+                See Product
+              </Button>
+            </article>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Recommended
