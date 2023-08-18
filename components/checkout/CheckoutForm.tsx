40c1f98fd8fb747a05cb28e91765edf0ce4a64bb
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-08-18 12:35:17 -0500

    feat(CheckoutForm.tsx): Implement 'use client' in CheckoutForm component
    The 'use client' feature to the CheckoutForm component, enabling enhanced interaction and dynamic
    behavior. The CheckoutForm component now manages payment methods, billing details, shipping
    information, and payment details, providing users with a streamlined and responsive checkout
    experience.

--- components/checkout/CheckoutForm.tsx
+++ components/checkout/CheckoutForm.tsx
@@ -1,5 +1,108 @@
+'use client'
+
+import { nanoid } from 'nanoid'
+import { useState } from 'react'
+
+import { billingDetails, eMoney, radioButtons, shippingInfo } from '@/constants'
+
+import Input from '../Input'
+import Radio from '../Radio'
+
 const CheckoutForm = () => {
-  return <div>CheckoutForm</div>
+  const [payMethod, setPayMethod] = useState('emoney')
+
+  return (
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="w-full max-w-[800px] lg:max-w-[730px]"
+    >
+      <div className="w-full rounded-lg bg-white px-6 md:px-[27.5px] lg:px-[48px]">
+        <h2 className="pt-[24px] text-[28px] font-bold md:pt-[30px] lg:pt-[58px]">
+          CHECKOUT
+        </h2>
+        <p className="mb-[16px] mt-[32px] font-bold text-orange-primary md:mt-[41px]">
+          BILLING DETAILS
+        </p>
+        <div className="flex flex-wrap gap-x-[16px] gap-y-[17px]">
+          {billingDetails.map((bd) => (
+            <div key={nanoid()} className="w-full md:w-max">
+              <Input
+                text={bd.placeholder}
+                type={bd.type}
+                errorMessage={bd.errorMessage}
+                require={bd.requirements}
+                name={bd.name}
+                valid={bd.required}
+              />
+            </div>
+          ))}
+        </div>
+        <p className="mb-[16px] mt-[32px] font-bold text-orange-primary md:mt-[53px]">
+          SHIPPING INFO
+        </p>
+        <div className="flex flex-wrap gap-x-[16px] gap-y-[24px]">
+          {shippingInfo.map((si) => (
+            <div key={nanoid()} className="w-full md:w-max">
+              <Input
+                text={si.placeholder}
+                type={si.type}
+                errorMessage={si.errorMessage}
+                require={si.requirements}
+                name={si.name}
+              />
+            </div>
+          ))}
+        </div>
+
+        <p className="mb-[16px] mt-[32px] font-bold text-orange-primary md:mt-[53px]">
+          PAYMENT DETAILS
+        </p>
+        <div className="flex max-w-[634px] flex-col md:flex-row md:justify-between">
+          <div>
+            <p className="mb-[9px] block text-[12px] font-bold tracking-[-0.21px]">
+              Payment Method
+            </p>
+          </div>
+          <div className="flex flex-col gap-[16px] pb-[30px]">
+            {radioButtons.map((radio) => (
+              <Radio
+                id={radio.id}
+                name={radio.name}
+                setPayMethod={setPayMethod}
+                payMethod={payMethod}
+                key={nanoid()}
+              />
+            ))}
+          </div>
+        </div>
+        {payMethod === 'emoney' && (
+          <div className="flex flex-wrap gap-x-[16px] gap-y-[24px] pb-[30px] lg:pb-[48px]">
+            {eMoney.map((em) => (
+              <div key={nanoid()} className="w-full md:w-max">
+                <Input
+                  text={em.placeholder}
+                  type={em.type}
+                  errorMessage={em.errorMessage}
+                  require={em.requirements}
+                  name={em.name}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+        {payMethod === 'cash' && (
+          <div className="flex flex-col items-center gap-[32px] pb-[30px] md:flex-row lg:pb-[48px]">
+            <img src="/checkout/icon-cash-on-delivery.svg" alt="" />
+            <p className="max-w-[554px] text-[15px] font-medium leading-[25px] opacity-50">
+              The ‘Cash on Delivery’ option enables you to pay in cash when our
+              delivery courier arrives at your residence. Just make sure your
+              address is correct so that your order will not be cancelled.
+            </p>
+          </div>
+        )}
+      </div>
+    </form>
+  )
 }
 
 export default CheckoutForm
--- constants/index.ts
+++ constants/index.ts
@@ -43,3 +43,104 @@ export const socials = [
     image: '/shared/desktop/icon-instagram.svg',
   },
 ]
+
+export const billingDetails = [
+  {
+    name: 'Name',
+    placeholder: 'Alexei Ward',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Name',
+    required: false,
+    requirements: '',
+  },
+  {
+    name: 'Email Address',
+    placeholder: 'alexei@mail.com',
+    type: 'email',
+    errorMessage: 'Wrong format',
+    label: 'Email',
+    required: false,
+    requirements: '@',
+  },
+  {
+    name: 'Phone Number',
+    placeholder: '+1 202-555-0136',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Phone Number',
+    required: false,
+    requirements: '',
+  },
+]
+
+export const shippingInfo = [
+  {
+    name: 'Your Address',
+    placeholder: '1137 Williams Avenue',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Username',
+    required: false,
+    requirements: '',
+  },
+  {
+    name: 'Zip Code',
+    placeholder: '10001',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Username',
+    required: false,
+    requirements: '',
+  },
+  {
+    name: 'City',
+    placeholder: 'New York',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Username',
+    required: false,
+    requirements: '',
+  },
+  {
+    name: 'Country',
+    placeholder: 'United States',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Username',
+    required: false,
+    requirements: '',
+  },
+]
+
+export const radioButtons = [
+  {
+    name: 'e-Money',
+    id: 'emoney',
+  },
+  {
+    name: 'Cash on Delivery',
+    id: 'cash',
+  },
+]
+
+export const eMoney = [
+  {
+    name: 'e-money Number',
+    placeholder: '238521993',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Username',
+    required: false,
+    requirements: '',
+  },
+  {
+    name: 'e-money PIN',
+    placeholder: '6891',
+    type: 'text',
+    errorMessage: 'plz fill out',
+    label: 'Username',
+    required: false,
+    requirements: '',
+  },
+]
